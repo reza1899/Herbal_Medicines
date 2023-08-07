fe50c135a322e4660b69a4aa1e6898bf36154176
reza1899 <reza.pk.lu9@gmail.com>
2023-08-07 13:05:03 -0700

    add rrd and some configs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Routes, Route} from "react-router-dom"
+import Main from "./components/index"
+const App = () => {
+    return (
+        <div className="App">
+           <Routes>
+               <Route path="/" element={<Main/>}/>
+           </Routes>
+        </div>
+    );
 }
-
 export default App;
---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import {useState} from "react";
+const Main = () => {
+    const [content, setContent] = useState("dfsd");
+    return (
+       <>
+           <div className="text-center mt-5">
+               <input className="w-50" type="search" value={content} onChange={(e) => setContent(e.target.value)}/>
+               <br />
+               {content}
+           </div>
+       </>
+    )
+}
+export default Main
