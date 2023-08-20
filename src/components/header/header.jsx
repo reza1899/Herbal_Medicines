db1f7ae1fc06dbee5b8f02244c72b3404bd8d543
reza1899 <reza.pk.lu9@gmail.com>
2023-08-20 13:21:22 -0700

    add responsive menu

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
             <header>
                 <nav className="">
                     <div className="container_nav">
-                    <div className="menu_icon">
-                          <button className="btn">
-                          <i className="fa fa-bars fs-5 m-2"></i>
-                          </button>
+                        <div className="menu_icon">
+                            <button className="btn">
+                                <i className="fa fa-bars fs-5 m-2"></i>
+                            </button>
                         </div>
                         <div className="logo">
                             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH6FrEVpGlltmCUHnuimao6-vEUKmSH4pIlg&usqp=CAU" alt="" />
@@ -29,6 +29,15 @@ const Navbar = () => {
                             </ul>
                         </div>
                     </div>
+                </nav>
+                <nav className="responsive_nav">
+                    <ul className="">
+                        <li><button className="btn" onClick={() => { navigate("/") }}>Home</button></li>
+                        <li><button className="btn" onClick={() => { navigate("/contact") }}>Contact</button></li>
+                        <li><button className="btn" onClick={() => { navigate("/blog") }}>Blog</button></li>
+                        <li><button className="btn" onClick={() => { navigate("/login") }}>Login</button></li>
+                        <li><button className="btn" onClick={() => { navigate("/register") }}>Register</button></li>
+                    </ul>
                 </nav>
             </header>
         </>
