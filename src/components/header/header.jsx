2565efe1d709e9f284f3c372c518eabd49a6cf33
reza1899 <reza.pk.lu9@gmail.com>
2023-08-23 10:11:41 -0700

    responsive menu is just created

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,7 +1,9 @@
 // import Search from "./searchBox"
 import { useNavigate } from "react-router-dom"
+import { useState } from "react"
 import "./header.css"
 const Navbar = () => {
+    const [show , setShow] = useState(false)
     const navigate = useNavigate()
     return (
         <>
@@ -9,7 +11,7 @@ const Navbar = () => {
                 <nav className="">
                     <div className="container_nav">
                         <div className="menu_icon">
-                            <button className="btn">
+                        <button className="btn" onClick={() => { setShow(!show) }}>
                                 <i className="fa fa-bars fs-5 m-2"></i>
                             </button>
                         </div>
@@ -30,15 +32,15 @@ const Navbar = () => {
                         </div>
                     </div>
                 </nav>
-                <nav className="responsive_nav">
-                    <ul className="">
-                        <li><button className="btn" onClick={() => { navigate("/") }}>Home</button></li>
-                        <li><button className="btn" onClick={() => { navigate("/contact") }}>Contact</button></li>
-                        <li><button className="btn" onClick={() => { navigate("/blog") }}>Blog</button></li>
-                        <li><button className="btn" onClick={() => { navigate("/login") }}>Login</button></li>
-                        <li><button className="btn" onClick={() => { navigate("/register") }}>Register</button></li>
-                    </ul>
-                </nav>
+            <div className="menu_div" style={{ display: show ? "block" : "none" }}>
+                    <ul className="menu_item">
+                            <li><button className="btn" onClick={() => { navigate("/") }}>Home</button></li>
+                            <li><button className="btn" onClick={() => { navigate("/contact") }}>Contact</button></li>
+                            <li><button className="btn" onClick={() => { navigate("/blog") }}>Blog</button></li>
+                            <li><button className="btn" onClick={() => { navigate("/login") }}>Login</button></li>
+                            <li><button className="btn" onClick={() => { navigate("/register") }}>Register</button></li>
+                        </ul>
+                    </div>
             </header>
         </>
     )
