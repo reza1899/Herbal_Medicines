b13ab4067aa6e0c26fb77c8ee2888a314bb4d396
reza1899 <reza.pk.lu9@gmail.com>
2023-09-07 22:32:17 -0700

    work on navbar yet

--- src/components/header/navbar/navbar.jsx
+++ src/components/header/navbar/navbar.jsx
@@ -1,13 +1,25 @@
 import "./navbar.css";
+
 const Navbar = () => {
     return (
         <>
-           <nav className="">
-              <div className="container">
-                  dfdfksdfk;sdf
-              </div>
-           </nav>
+            <nav className="">
+                <div className="container">
+                    <div className="nav_products_menu_icon" >
+                        <i className="fa fa-bars fs-5 mx-2 mt-1"></i>
+                        <p>نمایش محصولات</p>
+                    </div>
+
+                        <div className="nav_products_menu">
+                           <ul>
+                               <li>fdsdfsd</li>
+                               
+                           </ul>
+                        </div>
+                </div>
+            </nav>
         </>
     )
 }
+
 export default Navbar
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,31 +1,31 @@
-import {useContext} from "react"
-import contextApi from "./../context/contextApi"
+// import {useContext} from "react"
+// import contextApi from "./../context/contextApi"
 
 const Main = () => {
-    const {filteredPlants, searchValue} = useContext(contextApi)
+    // const {filteredPlants, searchValue} = useContext(contextApi)
     return (
         <>
-           <div className="1">
-               {
-                   searchValue ?
-                       (
-                           filteredPlants.map((plant) => {
-                               return <div className="card" key={plant.id}>
-                                   <div className="card-body">
-                                       <h5 className="card-title">{plant.name}</h5>
-                                       <p className="card-text">{plant.description}</p>
-                                   </div>
-                               </div>
-                           })
-                       )
-                       :
-                       (
-                           <div className="text-center">
-                               <h3>موردی یافت نشد</h3>
-                           </div>
+           <div className="">
+               {/*{*/}
+               {/*    searchValue ?*/}
+               {/*        (*/}
+               {/*            filteredPlants.map((plant) => {*/}
+               {/*                return <div className="card" key={plant.id}>*/}
+               {/*                    <div className="card-body">*/}
+               {/*                        <h5 className="card-title">{plant.name}</h5>*/}
+               {/*                        <p className="card-text">{plant.description}</p>*/}
+               {/*                    </div>*/}
+               {/*                </div>*/}
+               {/*            })*/}
+               {/*        )*/}
+               {/*        :*/}
+               {/*        (*/}
+               {/*            <div className="text-center">*/}
+               {/*                <h3>موردی یافت نشد</h3>*/}
+               {/*            </div>*/}
 
-                       )
-               }
+               {/*        )*/}
+               {/*}*/}
            </div>
         </>
     )
