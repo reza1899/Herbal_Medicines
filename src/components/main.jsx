66acddad5f2d4b0fca151ab538d277dd9bfe6d3e
reza1899 <reza.pk.lu9@gmail.com>
2023-09-09 23:15:25 -0700

    sweet section is done

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,21 +1,52 @@
 import "./main.css"
+
 const Main = () => {
-    return (
-        <>
-          <div>
-              <div className="our_story">
-                  <div className="container">
-                      <div className="story_desc">
-                          <h2>داروهای گیاهی</h2>
-                          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد،</p>
-                      </div>
-                      <div className="story_img">
-                          <img src="https://img.freepik.com/free-photo/love-blooms-heart-shaped-bouquet-flowers-generated-by-ai_188544-44585.jpg?t=st=1694323004~exp=1694326604~hmac=ffd53eab64371d39354b9359bd0148229371b30ffea045e58ab397723b32d2e1&w=1800" alt="" />
-                      </div>
-                  </div>
-              </div>
-          </div>
-        </>
-    )
+    return (<>
+            <div>
+                {/*  our-story section  */}
+                <div className="our_story">
+                    <div className="container">
+                        <div className="story_desc">
+                            <h2>داروهای گیاهی</h2>
+                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                                است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
+                                فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                                کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+                                طلبد،</p>
+                        </div>
+                        <div className="story_img">
+                            <img
+                                src="https://img.freepik.com/free-photo/love-blooms-heart-shaped-bouquet-flowers-generated-by-ai_188544-44585.jpg?t=st=1694323004~exp=1694326604~hmac=ffd53eab64371d39354b9359bd0148229371b30ffea045e58ab397723b32d2e1&w=1800"
+                                alt=""/>
+                        </div>
+                    </div>
+                </div>
+                {/*  end our-story section  */}
+
+                {/*  sweet section  */}
+
+                <div className="sweet">
+                   <div className="sweet_overlay">
+                       <div className="container">
+                           <div className="sweet_desc">
+                               <h1>داروهای گیاهی</h1>
+                               <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                                   است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
+                                   فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                                   کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+                                   طلبد،</p>
+                           </div>
+                       </div>
+                   </div>
+                </div>
+
+                {/*  end sweet section    */}
+                <div style={{ height: "300px" }}>
+
+
+                </div>
+
+            </div>
+        </>)
 }
 export default Main
