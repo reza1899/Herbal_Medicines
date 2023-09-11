4ef8e0cb79c7e61c44e4466bca93bb857dd6b114
reza1899 <reza.pk.lu9@gmail.com>
2023-09-10 19:44:24 -0700

    end menu section

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,51 +2,83 @@ import "./main.css"
 
 const Main = () => {
     return (<>
-            <div>
-                {/*  our-story section  */}
-                <div className="our_story">
+        <div>
+            {/*  our-story section  */}
+            <div className="our_story">
+                <div className="container">
+                    <div className="story_desc">
+                        <h2>داروهای گیاهی</h2>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                            است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
+                            فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                            کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+                            طلبد،</p>
+                    </div>
+                    <div className="story_img">
+                        <img
+                            src="https://img.freepik.com/free-photo/love-blooms-heart-shaped-bouquet-flowers-generated-by-ai_188544-44585.jpg?t=st=1694323004~exp=1694326604~hmac=ffd53eab64371d39354b9359bd0148229371b30ffea045e58ab397723b32d2e1&w=1800"
+                            alt="story_image"/>
+                    </div>
+                </div>
+            </div>
+            {/*  end our-story section  */}
+
+            {/*  sweet section  */}
+
+            <div className="sweet">
+                <div className="sweet_overlay">
                     <div className="container">
-                        <div className="story_desc">
-                            <h2>داروهای گیاهی</h2>
+                        <div className="sweet_desc">
+                            <h1>داروهای گیاهی</h1>
                             <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
                                 است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
                                 فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
                                 کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
                                 طلبد،</p>
                         </div>
-                        <div className="story_img">
-                            <img
-                                src="https://img.freepik.com/free-photo/love-blooms-heart-shaped-bouquet-flowers-generated-by-ai_188544-44585.jpg?t=st=1694323004~exp=1694326604~hmac=ffd53eab64371d39354b9359bd0148229371b30ffea045e58ab397723b32d2e1&w=1800"
-                                alt=""/>
-                        </div>
                     </div>
                 </div>
-                {/*  end our-story section  */}
-
-                {/*  sweet section  */}
-
-                <div className="sweet">
-                   <div className="sweet_overlay">
-                       <div className="container">
-                           <div className="sweet_desc">
-                               <h1>داروهای گیاهی</h1>
-                               <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
-                                   است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
-                                   فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
-                                   کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-                                   طلبد،</p>
-                           </div>
-                       </div>
-                   </div>
-                </div>
-
-                {/*  end sweet section    */}
-                <div style={{ height: "300px" }}>
-
-
-                </div>
-
             </div>
-        </>)
+
+            {/* menu section */}
+            <div className="menu">
+                <div className="container">
+                    <div className="menu_desc">
+                        <h1>داروهای گیاهی</h1>
+                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک
+                            است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط
+                            فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد،
+                            کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
+                            طلبد،</p>
+                    </div>
+                    <div className="menu_img">
+                        <ul>
+                            <li>
+                                <img
+                                    src="https://images.unsplash.com/photo-1655252205276-c4e210f36333?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+                                    alt="menu_image"/>
+                            </li>
+                            <li>
+                                <img
+                                    src="https://images.unsplash.com/photo-1531112606622-e8174567b048?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2588&q=80"
+                                    alt="menu_image"/>
+                            </li>
+                            <li>
+                                <img
+                                    src="https://images.unsplash.com/photo-1656501020056-1c631268e3d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2535&q=80"
+                                    alt="menu_image"/>
+                            </li>
+                            <li>
+                                <img
+                                    src="https://images.unsplash.com/photo-1659328376647-52ec39d1a5cf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2574&q=80"
+                                    alt="menu_image"/>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </>)
 }
 export default Main
