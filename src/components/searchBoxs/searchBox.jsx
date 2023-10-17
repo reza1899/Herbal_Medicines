df1b393bc38ed6acee43ae4e73ced82540f4c522
reza1899 <reza.pk.lu9@gmail.com>
2023-10-16 17:42:59 -0700

    search result div now is showed

--- src/components/searchBoxs/searchBox.jsx
+++ src/components/searchBoxs/searchBox.jsx
@@ -1,9 +1,21 @@
-import { useContext} from "react";
+import { useContext,useRef, useState } from "react";
 import {plants} from "../../service/data";
 import "./search.css";
 import contextApi from "./../../context/contextApi";
 
 const Search = () => {
+    const [searchResult, setSearchResult] = useState(false);
+    const inputRef = useRef(null);
+
+    const handleFocus = () => {
+        setSearchResult(true);
+    };
+
+    const handleBlur = () => {
+        setSearchResult(false);
+    };
+
+
     const {setFilteredPlants ,setSearchValue ,searchValue} = useContext(contextApi);
     let FilteredTimeout;
     const searching = () => {
@@ -20,14 +32,23 @@ const Search = () => {
             <div className="search-box text-center">
                 <i className="fas fa-search search-icon text-muted"></i>
                 <input
+                    ref={ inputRef }
                     className="search"
                     placeholder="جستجو کنید"
                     type="text"
                     onChange={event => searching(setSearchValue(event.target.value))}
-
+                    onFocus={handleFocus}
+                    onBlur={handleBlur}
                 />
                 <br/>
             </div>
+            {
+                searchResult ? (
+                    <div className="search-result">
+                        reza is good
+                    </div>
+                ) : null
+            }
         </>
     );
 };
