aae64c70a9d950b9bdb702305a7e5e4c3a1067b0
reza1899 <reza.pk.lu9@gmail.com>
2023-10-29 08:58:23 -0700

    Update blogs.jsx

--- src/components/blog/blogs.jsx
+++ src/components/blog/blogs.jsx
@@ -2,8 +2,8 @@ const Blogs = () => {
     return (
         <>
             <div>
-                <video className="position-absolute" style={{top: "30%", right:"25%"}} width="800" height="500" controls autoplay="autoplay">
-                    <source src="https://trello.com/1/cards/653c4fe91ac3dd661d48bfa8/attachments/653c4fea1ac3dd661d48bfef/download/ble.mp4" type="video/mp4" />
+                <video className="position-absolute" style={{top: "30%", right:"25%"}} width="800" height="500" controls >
+                    <source src="ble.mp4" type="video/mp4" />
                 </video>
 
             </div>
